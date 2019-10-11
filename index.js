e0f140fe8eca0db10017343814b0d7296885af38
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-11 17:06:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-var animal = 'dog'
-
 function myAnimal() {
+  var animal = 'dog'
   return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
   return animal
 }
 
-function add2(n) {
-  return n + two
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
 
-  // Feel free to move things around!
+function add2(n) {
   const two = 2
+  return n + two
 }
+
+ Feel free to move things around!
+
 
 var funkyFunction = function() {
   return function() {
